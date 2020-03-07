58137d8bba9260adfeeed490cd991dab289b0480
surryjm <50641921+surryjm@users.noreply.github.com>
2020-03-06 16:10:39 -0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GroceryListComponent } from './grocery-list/grocery-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroceryListComponent
   ],
   imports: [
     BrowserModule
