c5cd9aa00b870d955a250b3bb54ef3e2941abfdf
surryjm <50641921+surryjm@users.noreply.github.com>
2020-03-08 16:14:13 -0700

    initial styles, enter for search, included rows

--- src/app/grocery-list/grocery-list.component.ts
+++ src/app/grocery-list/grocery-list.component.ts
@@ -3,35 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-grocery-list',
   templateUrl: './grocery-list.component.html',
-  //styleUrls: ['./grocery-list.component.css']//
-  styleUrls: ['../../assets/bootstrap/css/bootstrap.min.css']
+  styleUrls: ['./grocery-list.component.css']
+  //styleUrls: ['../../assets/bootstrap/css/bootstrap.min.css']
 })
 export class GroceryListComponent implements OnInit {
   
   task: string;
-  /*task = {
-    name: '',
-    id: 0
-  };*/
   tasks = [];
 
   //Add task
   onClick() {
    this.tasks.push({name: this.task});
     this.task = '';
-    /*if(this.task.id == 0){
-      this.tasks.push({id: (new Date()).getTime(),name: this.task.name});
-    }*/
   }
-
-  /*taskSubmit(value:any){
-    if(value!==""){
-      this.tasks.push(value.task)
-    }
-    else{
-      alert('Field required')
-    }
-  }*/
 
 
   //Delete task
