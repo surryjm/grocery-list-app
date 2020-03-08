ecc5c194179e7ee213664f0fc545332d211fb6f8
surryjm <50641921+surryjm@users.noreply.github.com>
2020-03-07 23:58:24 -0800

    Added material icons, delete task functionality

--- src/app/grocery-list/grocery-list.component.ts
+++ src/app/grocery-list/grocery-list.component.ts
@@ -9,11 +9,38 @@ import { Component, OnInit } from '@angular/core';
 export class GroceryListComponent implements OnInit {
   
   task: string;
+  /*task = {
+    name: '',
+    id: 0
+  };*/
   tasks = [];
 
+  //Add task
   onClick() {
-    this.tasks.push({name: this.task});
+   this.tasks.push({name: this.task});
     this.task = '';
+    /*if(this.task.id == 0){
+      this.tasks.push({id: (new Date()).getTime(),name: this.task.name});
+    }*/
+  }
+
+  /*taskSubmit(value:any){
+    if(value!==""){
+      this.tasks.push(value.task)
+    }
+    else{
+      alert('Field required')
+    }
+  }*/
+
+
+  //Delete task
+  deleteTask(task){
+    for(let i=0; i<= this.tasks.length; i++){
+      if(task==this.tasks[i]){
+        this.tasks.splice(i,1)
+      }
+    }
   }
 
   constructor() { }
