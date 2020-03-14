b2215d33a888c06891e5a71a7af4bc3f1e65dfa8
surryjm <50641921+surryjm@users.noreply.github.com>
2020-03-13 22:56:06 -0700

    App component spec fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('grocery-list-app');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('grocery-list-app app is running!');
-  });
 });
